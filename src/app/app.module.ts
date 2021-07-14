d1971e63ee43e066d2c39764c2157ef03f83bfc7
AntonioJuresic <ajuresic@tvz.hr>
2021-07-14 18:21:10 +0200

    Dodane boje i gumbovi.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StylingComponent } from './styling/styling.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StylingComponent
   ],
   imports: [
     BrowserModule,
