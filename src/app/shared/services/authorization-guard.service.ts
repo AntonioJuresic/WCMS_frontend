50948a9af369efee540ed58fa59dc42cb50b9e83
AntonioJuresic <ajuresic@tvz.hr>
2021-10-06 16:49:44 +0200

    Riješen problem s authguardom (ponovno).

--- src/app/shared/services/authorization-guard.service.ts
+++ src/app/shared/services/authorization-guard.service.ts
@@ -8,23 +8,16 @@ import { AuthenticationService } from './authentication.service';
 })
 export class AuthorizationGuardService {
 
-    public userIsLogged: boolean = false;
     authenticationSubscription: Subscription = new Subscription;
 
     constructor(
         private authenticationService: AuthenticationService,
         private router: Router
-    ) {
-        /*this.authenticationService.isUserAuthenticated();
-
-        this.authenticationSubscription = this.authenticationService.isAuthenticatedObservable
-            .subscribe(res => {
-                this.userIsLogged = res;
-                console.log("this.userIsLogged - ", this.userIsLogged);
-            })*/
-    }
+    ) { }
 
     needsAuthentication() {
+        this.authenticationService.isUserAuthenticated();
+
         this.authenticationService.isAuthenticatedObservable
             .subscribe(res => {
                 if (!res) {
@@ -34,6 +27,8 @@ export class AuthorizationGuardService {
     }
 
     canNotAcessAuthenticated() {
+        this.authenticationService.isUserAuthenticated();
+
         this.authenticationService.isAuthenticatedObservable
             .subscribe(res => {
                 if (res) {
